002b29661913ea6e0fd5c825d1e0d0f43ac58917
taroyanaka <ababbbccc@gmail.com>
2018-10-21 10:17:02 +0900

    anyway commit

--- avascript.js
+++ avascript.js
@@ -1,35 +1,6 @@
 var HERE = `
 [1,2,3]
 map *2
-`
-
-var REPLACEDHERE = HERE.replace(/ /g,"(v=>v").replace(/map/g,".map").replace(/filter/g,".filter").replace(/^\..*$/g,")").replace(/$/g,")")
-
-document.write(REPLACEDHERE)
-document.write( Function('"use strict";return (' + REPLACEDHERE + ')')() )
-
-document.write( "\n" )
-
-var HERE = `
-[10,20,30]
-filter ==20
-`
-
-var REPLACEDHERE = HERE.replace(/ /g,"(v=>v").replace(/map/g,".map").replace(/filter/g,".filter").replace(/^\..*$/g,")").replace(/$/g,")")
-
-document.write(REPLACEDHERE)
-document.write( Function('"use strict";return (' + REPLACEDHERE + ')')() )
-
-
-
-
-
-
-
-
-var HERE = `
-[1,2,3]
-map *2
 map *2
 map *2
 filter ==16`
