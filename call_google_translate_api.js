9bfd595b619ebf71333a9c4ca3f6679be3370157
taroyanaka <ababbbccc@gmail.com>
2018-11-05 20:34:21 +0900

    anyway commit

--- call_google_translate_api.js
+++ call_google_translate_api.js
@@ -1,3 +1,5 @@
+// https://qiita.com/tanabee/items/c79c5c28ba0537112922
+// https://aspnet-mvc.clock-up.jp/contents/rest-api/access-by-js#GET_%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B9%E4%BE%8B
 // shut out other api users done
 fetch('https://script.google.com/macros/s/AKfycbwK7J2zrpdVBXLaFlreHTXSYCabLuoEiCzuTWKkxjp_flZR86M/exec?text=こんにちわ&source=ja&target=en')
     .then(function (response) {
