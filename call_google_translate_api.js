fd67717a37710506b8cacbe1f808702dd63780bd
taroyanaka <ababbbccc@gmail.com>
2018-11-05 20:45:10 +0900

    anyway commit

--- call_google_translate_api.js
+++ call_google_translate_api.js
@@ -2,8 +2,8 @@
 // https://aspnet-mvc.clock-up.jp/contents/rest-api/access-by-js#GET_%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B9%E4%BE%8B
 // shut out other api users done
 fetch('https://script.google.com/macros/s/AKfycbwK7J2zrpdVBXLaFlreHTXSYCabLuoEiCzuTWKkxjp_flZR86M/exec?text=こんにちわ&source=ja&target=en')
-    .then(function (response) {
-        return response.text();
-    }).then(function (text) {
+    .then(res => {
+        return res.text();
+    }).then(text => {
         console.log(text); // hello
     });
