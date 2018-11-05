ab64b55c1a7a449d3bd101096b0e4a7ebfb076e8
taroyanaka <ababbbccc@gmail.com>
2018-11-05 20:22:15 +0900

    anyway commit

--- call_google_translate_api.js
+++ call_google_translate_api.js
@@ -1,5 +1,7 @@
-fetch('https://script.google.com/macros/s/AKfycbwK7J2zrpdVBXLaFlreHTXSYCabLuoEiCzuTWKkxjp_flZR86M/exec?text=こんにちわ&source=ja&target=en').then(function (response) {
-    return response.text();
-}).then(function (text) {
-    console.log(text);
-});
+// shut out other api users done
+fetch('https://script.google.com/macros/s/AKfycbwK7J2zrpdVBXLaFlreHTXSYCabLuoEiCzuTWKkxjp_flZR86M/exec?text=こんにちわ&source=ja&target=en')
+    .then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        console.log(text);
+    });
