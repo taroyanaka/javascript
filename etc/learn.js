4d1aa66b46de649024456a4ff8b6200253fb851b
taroyanaka <ababbbccc@gmail.com>
2019-01-15 22:36:46 +0900

    anyway commit

--- etc/learn.js
+++ etc/learn.js
@@ -12,3 +12,39 @@ const simpleCrawler = (url = "https://www.yahoo.co.jp/") => {
 }
 
 simpleCrawler();
+
+
+
+const request = require('request');
+//const HTMLParser = require('fast-html-parser');
+const HTMLParser = require('htmlparser2');
+request('https://www.yahoo.co.jp/', (error, response, body) => { 'body:', 
+        HTMLParser.parse(body)
+        .querySelectorAll('a')
+//        .forEach(aElm => { console.log(aElm.textContent) })
+});
+[1, 2, 3].forEach(v => { console.log(v) });
+
+
+const request = require('request'); let foo = [];
+const HTMLParser = require('fast-html-parser');
+request('https://www.yahoo.co.jp/', (error, response, body) => {
+    'body:',
+    foo = HTMLParser.parse(body)
+        .querySelectorAll('a')
+});
+
+Array.from(foo).forEach(v => { console.log(v) });
+
+// const bar = Array.from(foo);
+// bar.forEach(v => { console.log(v) });
+
+
+
+const request = require('request'); let target = [];
+const HTMLParser = require('fast-html-parser');
+request('https://www.yahoo.co.jp/', (error, response, body) => { //console.log(
+    HTMLParser.parse(body)
+        .querySelectorAll('a').forEach(v => { target.push(v.rawAttrs) })
+    //)
+});
