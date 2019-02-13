cd912bcefbc1e3b32eadef684311be79716ec8fe
taroyanaka <ababbbccc@gmail.com>
2019-02-13 14:54:26 +0900

    anyway commit

--- etc/withnode/firestore.js
+++ etc/withnode/firestore.js
@@ -2,7 +2,7 @@
 
 const admin = require('firebase-admin');
 admin.initializeApp({
-  credential: admin.credential.cert(require('/Users/yanakataro/googleDrive/javascript/HTML/firebase/yanaka-7bb50.json'))
+  credential: admin.credential.cert(require('/Users/yanakataro/Desktop/javascript/HTML/firebase/yanaka-7bb50.json'))
 });
 
 admin.firestore().settings({ timestampsInSnapshots: true });
@@ -16,3 +16,9 @@ admin.firestore().collection('users').get()
   .catch((err) => {
     console.log('Error getting documents', err);
   });
+
+if (Array.from(palindrome).reduceRight((r, k) => r += k) === Array.from(palindrome).reduce((r, k) => r += k)) {
+  "OK"
+} else {
+  "NG"
+};
