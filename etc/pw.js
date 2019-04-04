9184d76d10534bdbea4dd947f6d6f4385d54bc76
taroyanaka <ababbbccc@gmail.com>
2019-04-05 03:20:24 +0900

    anyway commit

--- etc/pw.js
+++ etc/pw.js
@@ -23,7 +23,7 @@ self.addEventListener('install', function (event) {
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches
-        .match(event.request)
+        .match(event.request)  
         .then(function (response) {
             return response ? response : fetch(event.request);
         })
