af5058377bb4b96ec5671fcfb3793ca82afb444d
taroyanaka <ababbbccc@gmail.com>
2019-04-04 18:47:24 +0900

    anyway commit

--- etc/pw.js
+++ etc/pw.js
@@ -20,12 +20,12 @@ self.addEventListener('install', function (event) {
 });
 
 // リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches
-        .match(event.request)
-        .then(function (response) {
-            return response ? response : fetch(event.request);
-        })
-    );
-});
+// self.addEventListener('fetch', function (event) {
+//     event.respondWith(
+//         caches
+//         .match(event.request)
+//         .then(function (response) {
+//             return response ? response : fetch(event.request);
+//         })
+//     );
+// });
