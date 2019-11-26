0bc916fa0e3d87170ce208a1e5729cec92a5c61f
taroyanaka <ababbbccc@gmail.com>
2019-11-26 16:32:47 +0900

    anyway commit

--- links/googleImageListTemplateGenerator.js
+++ links/googleImageListTemplateGenerator.js
@@ -1,21 +1,16 @@
 const getLines = (event) =>{
-let NAMES = event.target.value;
-
-
-
-// NAMES = `Nadir Afonso
-// Yaacov Agam
-// Larry Zox
-// Jan Zrzavý`
-
-
-let HEADER = `<div><a href="https://www.google.com/search?q=`
-let FOOTER = `&tbm=isch">`
-let ATAG = `</a></div>\n`
-let URLS = R.pipe(R.split('\n'), R.map(R.replace(/ /g, '+')), R.map(R.replace(/^/g, HEADER)), R.map(R.replace(/$/g, FOOTER)))(NAMES);
-let ALLNAMES = R.pipe(R.split('\n'), R.map(R.replace(/$/g, ATAG)))(NAMES);
-let URLSWITHNAMES = R.pipe(R.transpose, R.flatten, R.join(""))([URLS, ALLNAMES])
-let DATA = `<!DOCTYPE html>
+	const NAMES = event.target.value;
+	// NAMES = `Nadir Afonso
+	// Yaacov Agam
+	// Larry Zox
+	// Jan Zrzavý`
+	const HEADER = `<div><a href="https://www.google.com/search?q=`
+	const FOOTER = `&tbm=isch">`
+	const ATAG = `</a></div>\n`
+	const URLS = R.pipe(R.split('\n'), R.map(R.replace(/ /g, '+')), R.map(R.replace(/^/g, HEADER)), R.map(R.replace(/$/g, FOOTER)))(NAMES);
+	const ALLNAMES = R.pipe(R.split('\n'), R.map(R.replace(/$/g, ATAG)))(NAMES);
+	const URLSWITHNAMES = R.pipe(R.transpose, R.flatten, R.join(""))([URLS, ALLNAMES])
+	document.querySelector("#bar").value = `<!DOCTYPE html>
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 		<script src="//cdnjs.cloudflare.com/ajax/libs/ramda/0.26.1/ramda.min.js"></script>
@@ -25,8 +20,7 @@ let DATA = `<!DOCTYPE html>
 	<button onclick="openTab();">If you allow pop-ups, you can open all tabs with this button</button>
 	<script type="text/javascript">
 	function openTab(){
-let NAMES = \`${NAMES}\`
-
+	const NAMES = \`${NAMES}\`
 	const HEADER = \`https://www.google.com/search?q=\`
 	const FOOTER = \`&tbm=isch\`
 	const URLS = R.pipe(R.split('\\n'), R.map(R.replace(/ /g, '+')), R.map(R.replace(/^/g, HEADER)), R.map(R.replace(/$/g, FOOTER)))(NAMES);
@@ -40,6 +34,4 @@ let NAMES = \`${NAMES}\`
 ${URLSWITHNAMES}
 </body>
 </html>`
-document.querySelector("#bar").value = DATA;
-
 };
