0e710026d0d439e02e13b17940baf7c1be70e535
taroyanaka <ababbbccc@gmail.com>
2020-04-08 19:21:05 +0900

    anyway commit

--- etc/withnode/tmp5.js
+++ etc/withnode/tmp5.js
@@ -4,6 +4,26 @@
 
 const R = require(`ramda`);
 
-R.replace(/a/g, "b", "abc");
 
+R.test(/^x/, 'xyz'); //=> true
+R.test(/^y/, 'xyz'); //=> false
 
+const a = R.filter(R.test(/x/gi), ["abc", "axb", "xb", "xb", "axax", "XA"]); 
+const b = R.filter(R.test(/x/g), ["abc", "axb", "xb", "xb", "axax", "XA"]); 
+const c = R.filter(R.test(/x/i), ["abc", "axb", "xb", "xb", "axax", "XA"]); 
+// console.table([a, b, c])
+
+const makeDescendList = (array) => {
+	return R.pipe(
+		R.countBy(R.identity),
+		R.toPairs,
+		R.sort(R.descend(R.prop(1))),
+		R.pluck(0),
+		R.reject(R.isEmpty),
+		R.reject(R.isNil)
+	)(array);
+}
+
+const list = ["hospital", "chairperson", "table", "agenda", ""]
+const res = makeDescendList(list);
+console.log(res);
