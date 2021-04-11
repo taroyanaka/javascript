1fd517760c0a0fd164fae01929e8d35760a845cf
taroyanaka <ababbbccc@gmail.com>
2021-04-11 13:34:05 +0900

    any way

--- HTML/tmp.sql
+++ HTML/tmp.sql
@@ -1,19 +1,5 @@
-create table tableA
-(
-tableA_id tinyint(3) not null auto_increment,
-last_update timestamp not null,
-primary key(tableA_id)
-);
-
-
-create table tableB
-(
-tableB_id tinyint(3) not null auto_increment,
-tableA_id tinyint(3),
-last_update timestamp, 
-primary key(tableB_id),
-foreign key(tableA_id) references tableA(tableA_id)
-);
+create table tableA(tableA_id tinyint(3) not null auto_increment,last_update timestamp not null,primary key(tableA_id));
+create table tableB(tableB_id tinyint(3) not null auto_increment,tableA_id tinyint(3),last_update timestamp, primary key(tableB_id),foreign key(tableA_id) references tableA(tableA_id));
 
 
 INSERT INTO tableA VALUES (1,'2006-02-15 04:34:33'),(2,'2006-02-15 04:34:33'),(3,'2006-02-15 04:34:33'),(4,'2006-02-15 04:34:33'),(5,'2006-02-15 04:34:33');
