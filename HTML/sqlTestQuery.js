bbea558140fe22edf5c7347f9fb93c41514f23c0
taroyanaka <ababbbccc@gmail.com>
2021-05-06 15:31:37 +0900

    any way

--- HTML/sqlTestQuery.js
+++ HTML/sqlTestQuery.js
@@ -1,7 +1,8 @@
 // https://paiza.io/en/projects/new?language=mysql
 // browser console
 
-const sqlTestQuery = `select * from actor;
+const sqlTestQuery = `SELECT * FROM actor;
+select flm.title, count(*) number_of_actors from film flm inner join film_actor fim_act on flm.film_id = fim_act.film_id group by flm.title order by number_of_actors desc;
 SELECT * FROM category;
 SELECT * FROM city;
 SELECT * FROM country;
