b7a8739a3739f8b1ab43ea08897c8b4c3024a2c4
taroyanaka <ababbbccc@gmail.com>
2022-03-27 11:28:01 +0900

    anyway

---
+++ HTML/tmp2_sw.js
@@ -0,0 +1,35 @@
+// https://github.com/webhacck/pwa-basic-sample
+var CACHE_NAME = 'cache-v1';
+
+var urlsToCache = [
+  '/',
+  'tmp2_manifest.json',
+  'tmp2-icon.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {  
+  event.waitUntil(
+    caches.keys().then(function(cache) {
+      cache.map(function(name) {
+        if(CACHE_NAME !== name) caches.delete(name);
+      })
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(res) {
+        if(res) return res;
+        return fetch(event.request);
+    })
+  );
+});
