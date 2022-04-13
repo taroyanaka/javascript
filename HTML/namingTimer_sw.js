b1603d84faa8bb85c6192463ccdcb77859acc273
taroyanaka <ababbbccc@gmail.com>
2022-04-13 14:50:35 +0900

    any way

--- HTML/namingTimer_sw.js
+++ HTML/namingTimer_sw.js
@@ -1,11 +1,11 @@
-const version = 'v1';
-self.addEventListener('install', (event) => {
+const VERSION = 'v3';
+self.addEventListener('install', event => {
   console.log('service worker install ...');
   event.waitUntil(
-    caches.open('v1').then((cache) => {
+    caches.open(VERSION).then((cache) => {
       return cache.addAll([
-        './hogehoge',
-        // './namingTimer.html',
+        // './hogehoge',
+        './namingTimer.html',
         './manifest.json',
         './validator.13.7.0.min.js',
         './1.png',
@@ -13,13 +13,14 @@ self.addEventListener('install', (event) => {
     })
     .then(res=>  console.log("foo1") )
   );
-  console.log("foo2");
+  // event.waitUntil(self.skipWaiting());
 });
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', async event => {
+  // await event.waitUntil(self.clients.claim());
   console.info('activate', event);
   console.log("bar");
 });
-// self.addEventListener('fetch', function(event) {
+// self.addEventListener('fetch', event => {
 //   console.log('fetch', event.request.url);
 //   console.log("buz");
 //   event.respondWith(
@@ -36,6 +37,7 @@ self.addEventListener('activate', (event) => {
 
 
 
+
 self.addEventListener('message', e => {
   console.log(`I got a message from browser. ${e.data}`);
 });
@@ -46,23 +48,31 @@ self.addEventListener('message', e => {
 //   console.log("foo")
 // })
 
-self.addEventListener('push', e => {
-  e.ports[0].postMessage({
-    msg: "Hey I just got a push from you!",
-    data: e.data
-  });
+self.addEventListener('push', event => {
+  event.waitUntil(
+    (async () => {
+      event.ports[0].postMessage({
+        msg: "Hey I just got a push from you!",
+        data: e.data
+      });
+    }))
 });
 // caches.keys().forEach(cache=>caches.delete(cache));
 
 
 addEventListener('fetch', event => {
-  event.waitUntil(async function() {
-    if (!event.clientId) return;
-    const client = await clients.get(event.clientId);
-    if (!client) return;
-    client.postMessage({
-      msg: "私はあなたからフェッチされましたよ！",
-      url: event.request.url
-    });
-  }());
+  event.waitUntil(
+    (async () => {
+      if (!event.clientId) return;
+      if (!(await clients.get(event.clientId))) return;
+      (await clients.get(event.clientId)).postMessage({
+        msg: "fetch!",
+        url: event.request.url
+      });
+    })()
+  )
+  // .then(V=>console.log("fetch done!"));
 });
+
+
+// console.log((await navigator.serviceWorker.ready).waiting);
