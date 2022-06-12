0ad3973642ac46cd23ad1afa95732af860394dec
taroyanaka <ababbbccc@gmail.com>
2022-06-13 02:41:17 +0900

    any way

--- HTML/pwa-builder-test3/node_express_cors_better-sqlite3_server.js
+++ HTML/pwa-builder-test3/node_express_cors_better-sqlite3_server.js
@@ -21,6 +21,27 @@ const port = 8800
 const Database = require('better-sqlite3');
 const db = new Database('.data/tmp2.sqlite3');
 
+const tag_create_table = () => `CREATE TABLE IF NOT EXISTS tag_table (
+id INT,
+no_filter_list_table_tag_table_id INT,
+tag TEXT)`;
+const no_filter_list_table_tag_table_create_table = () => `CREATE TABLE IF NOT EXISTS no_filter_list_table_tag_table (
+id INT,
+no_filter_list_table_id INT,
+tag_table_id INT)`;
+const comment_create_table = () => `CREATE TABLE IF NOT EXISTS comment_table (
+id INT,
+no_filter_list_table_id INT,
+comment TEXT)`;
+const no_filter_list_create_table = () => `CREATE TABLE IF NOT EXISTS no_filter_list_table (
+id INT,
+article TEXT,
+star_count INT,
+comment_count INT,
+article_length INT,
+match_score NUMBER
+)`;
+
 const create = async () => {
     db.prepare(`CREATE TABLE IF NOT EXISTS service (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,14 +55,54 @@ service_id INTEGER NOT NULL,
 FOREIGN KEY (service_id) REFERENCES service(id)
 );`).run();
 };
+const create_query_exe = async (STR) => db.prepare(STR).run();
 
-const insertService = () => {
-    const stmt = db.prepare('INSERT INTO service (service) VALUES (?)');
-    stmt.run("service0");
-    stmt.run("service1");
-    stmt.run("service2");
-    stmt.run("anysql");
-    stmt.run("grouppicker");
+const table_name_key_value_1 = {article_lists_table: [
+["id", 1],
+["article", "foo1"],
+["search_txt", "foo1"],
+["sort_by", "foo1"],
+["sort_asc_or_desc", true],
+["editing", 0],
+["star_count", 1],
+["comment_count", 3],
+["article_length", 3],
+["match_score", 0],
+]}
+
+const table_name_key_value_2 = {no_filter_list_table: [
+["id", 1],
+["article", "foo0"],
+["star_count", 3],
+["comment_count", 1],
+["article_length", 4],
+["match_score", 0.1],
+]}
+
+const table_name_key_value_3 = {no_filter_list_table: [
+["id", 2],
+["article", "bar"],
+["star_count", 0],
+["comment_count", 1],
+["article_length", 3],
+["match_score", 0.2],
+]}
+
+const get_key = (key_val_array) => [key_val_array.map(V=>V[0]), key_val_array.map(V=>V[1])][0].join(" ");
+const get_value = (key_val_array) => [key_val_array.map(V=>V[0]), key_val_array.map(V=>V[1])][1].map(V=>typeof V ==='string' ? "'" + V + "'" : V).join(" ");
+
+const insert_initial_data = (table_name_key_value) => {
+    const table_name = Object.keys(table_name_key_value)[0];
+    const key_val = Object.values(table_name_key_value)[0];
+    const KEY_VAL = [key_val.map(V=>V[0]), key_val.map(V=>V[1])][1].join(" ");
+    const stmt = db.prepare(`
+INSERT INTO ${table_name} (
+${KEY_VAL[0].join(" ")}
+)VALUES (
+    ${KEY_VAL[1].map(V=>typeof V ==='string' ? "'" + V + "'" : V).map(V=> "?").join(" ")}
+)`);
+    // stmt.run("1 'foo1' 'foo1' 'foo1' true 0 1 3 3 0");
+    stmt.run(KEY_VAL[1].join(" "));
 };
 
 const insertAny = () => {
