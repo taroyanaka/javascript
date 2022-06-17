64daceec07104a48b38c1b08538520ea4e8c6612
taroyanaka <ababbbccc@gmail.com>
2022-06-17 18:41:29 +0900

    any way

--- HTML/pwa-builder-test3/test.sql
+++ HTML/pwa-builder-test3/test.sql
@@ -1,89 +1,3 @@
--- const blog = {
---     "no_filter_list": [],
---     "search_txt_txt": "",
---     "sort_by": "star_count",
---     "sort_asc_or_desc": false,
---     "editing": 0,
---     "list": [
---         {
---             "id": 0,
---             "article": "foo0",
---             "tag_list": [
---                 "FOO",
---                 "BAR"
---             ],
---             "comment_list": [
---                 {
---                     "comment": "foo is FOO"
---                 },
---                 {
---                     "comment": "foo is not BAR"
---                 },
---                 {
---                     "comment": "foo2 is FOO2"
---                 }
---             ],
---             "star_count": 3,
---             "comment_count": 3,
---             "article_length": 4,
---             "match_score": 0
---         },
---         {
---             "id": 2,
---             "article": "foo\nbar\nBUZ",
---             "tag_list": [
---                 "BAR",
---                 "QUX"
---             ],
---             "comment_list": [],
---             "star_count": 1,
---             "comment_count": 0,
---             "article_length": 11,
---             "match_score": 0
---         },
---         {
---             "id": 1,
---             "article": "bar",
---             "tag_list": [
---                 "QUX"
---             ],
---             "comment_list": [
---                 {
---                     "comment": "bar is BAR"
---                 }
---             ],
---             "star_count": 0,
---             "comment_count": 1,
---             "article_length": 3,
---             "match_score": 0
---         },
---         {
---             "id": 3,
---             "article": "vlahvlah!",
---             "tag_list": [],
---             "comment_list": [],
---             "star_count": 0,
---             "comment_count": 0,
---             "article_length": 9,
---             "match_score": 0
---         },
---         {
---             "id": 4,
---             "article": "日本語　にニほんホンゴご　ﾆﾎﾝｺﾞ",
---             "tag_list": [],
---             "comment_list": [],
---             "star_count": 0,
---             "comment_count": 0,
---             "article_length": 18,
---             "match_score": 0
---         }
---     ],
---     "tmpList": null,
---     "selected": "",
---     "tag_filter_with_OR_selection": []
--- }
-
-
 -- GitHub Copilot write
 CREATE TABLE IF NOT EXISTS list (
     id INTEGER PRIMARY KEY,
@@ -95,6 +9,30 @@ CREATE TABLE IF NOT EXISTS list (
     article_length INTEGER,
     match_score INTEGER
 );
+
+-- const article_lists = {
+--     "no_filter_list": [],
+--     "search_txt_txt": "",
+--     "sort_by": "star_count",
+--     "sort_asc_or_desc": false,
+--     "editing": 0,
+--     "list": [
+--     ],
+--     "tmpList": null,
+--     "selected": "",
+--     "tag_filter_with_OR_selection": []
+-- }
+
+CREATE TABLE IF NOT EXISTS article_lists (
+    id INTEGER PRIMARY KEY,
+    no_filter_list TEXT,
+    search_txt_txt TEXT,
+    sort_by TEXT,
+    sort_asc_or_desc INTEGER,
+    editing INTEGER,
+    tmpList TEXT,
+    selected TEXT,
+)
 
 -- vue_instance.$data copy & paste
 -- const vue_instance = {
