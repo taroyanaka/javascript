d796b4fce7b312fc4901b7e52701796d709eebeb
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2022-06-23 07:29:50 +0900

    any way

---
+++ vue_pack/a/src/Foo.vue
@@ -0,0 +1,29 @@
+<template>
+<h1 id="bar" @click="plus_counter">BAR</h1>
+<button @click="plus_counter">ABC</button>
+<div>
+  {{counter}}
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      counter: 10
+    }
+  },
+  methods: {
+    plus_counter() {
+      this.counter++
+    }
+  }
+}
+</script>
+
+<style>
+#bar {
+  width: 10rem;
+  height: 10rem;
+  background: gray;
+}
+</style>
---
+++ vue_pack/a/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+// import App from './App.vue'
+import Foo from './Foo.vue'
+
+// createApp(App).mount('#app')
+createApp(Foo).mount('#foo')
